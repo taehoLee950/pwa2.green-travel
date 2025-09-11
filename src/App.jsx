343803a5731e43b07751873615093b96f6bdc9f7
taehoLee950 <inwoo950@gmail.com>
2025-09-11 12:59:15 +0900

    250911 branch test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lemon fresh Taeho</h1>
+      <h1>Organic Lemon</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
